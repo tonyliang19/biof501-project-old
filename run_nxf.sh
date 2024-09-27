3eb559e964ce0d4be7cb6b1dc5d42ec71efc8e12
tonyliang19 <chunqingliang@gmail.com>
2024-09-26 20:50:20 -0700

    rewrite it to accept more variable command for running nxf, allow for svaing log in file

--- run_nxf.sh
+++ run_nxf.sh
@@ -7,17 +7,26 @@
 
 # Configuration options
 NXF_SRC_MAIN=main.nf # Main script to run
+BATCH=FALSE # By default logs are printed into the screen, if true then to log
 RESUME=FALSE # Initially run without resume
+# Add unique timestamp to the log file
+timestamp=$(date +"%Y%m%d_%H%M%S")
+LOG_FILE=${timestamp}-DGE_pipeline.log
 OUTDIR=results # Output directory for publishing files
 
 # Add a parser from cli arg i.e.
 # bash run_nxf.sh -r allows for resuming workflow
-while getopts ":r" option;  # include both 'r' and 's' options
+# bahs run_nxf.sh -b allows for redireting output of the log to DGE_pipeline.log
+while getopts ":rb" option;  # include both 'r' and 'b' options
 do 
   case $option in
   r)
     echo -e "Resuming pipeline\n"
     RESUME=TRUE
+    ;;
+  b)
+    echo -e "Running as batch job mode, log will be output to ${LOG_FILE}\n"
+    BATCH=TRUE
     ;;
   *)
     echo "Invalid option: -$OPTARG"
@@ -26,17 +35,27 @@ do
   esac
 done
 
-# Always make sure to create the output directory
-#mkdir -p $OUTDIR
+# Then parse the run cmd
+NEXTFLOW_CMD="nextflow run ${NXF_SRC_MAIN} --outdir ${OUTDIR}"
 
-if [ "${RESUME}" == "TRUE" ];
-then
-  # Only when provide -r in cli, then run the pipeline with resume
-  nextflow run ${NXF_SRC_MAIN} \
-  --outdir ${OUTDIR} \
-  -resume
-else
-  # Otherwise run without resume
-  nextflow run ${NXF_SRC_MAIN} \
-  --outdir ${OUTDIR}
+if [ "$RESUME" == "TRUE" ]; then
+  NEXTFLOW_CMD+=" -resume"
 fi
+
+if [ "$BATCH" == "TRUE" ]; then
+  NEXTFLOW_CMD+=" -ansi-log false > ${LOG_FILE} 2>&1"  # Redirect output and errors to the log file
+fi
+
+# Then execute the run cmd
+eval ${NEXTFLOW_CMD}
+# if [ "${RESUME}" == "TRUE" ];
+# then
+#   # Only when provide -r in cli, then run the pipeline with resume
+#   nextflow run ${NXF_SRC_MAIN} \
+#   --outdir ${OUTDIR} \
+#   -resume
+# else
+#   # Otherwise run without resume
+#   nextflow run ${NXF_SRC_MAIN} \
+#   --outdir ${OUTDIR}
+# fi
